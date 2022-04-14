0ea525d9c47fbf74f053ff1d8dd2133eb3303416
Luiz <lhvalentim@outlook.coom>
2022-04-14 09:23:24 -0300

    fix baseUrl

---
+++ precache-manifest.15db1459f693624470068ac233dea6be.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a70fbc19db64f6c428",
+    "url": "/poker/css/app~d0ae3f07.7d1dd63b.css"
+  },
+  {
+    "revision": "12d0649bf2c077cd8f7f",
+    "url": "/poker/css/chunk-d3536006.6d89d1db.css"
+  },
+  {
+    "revision": "dec24c0ef0a49e7528a7",
+    "url": "/poker/css/chunk-vendors~11c2601a.94c0a673.css"
+  },
+  {
+    "revision": "1669a9d1ad9e46a05259",
+    "url": "/poker/css/chunk-vendors~c6230548.51b8982f.css"
+  },
+  {
+    "revision": "5f9dcb3fad9472b3e9cfe4fa1ef3a4f4",
+    "url": "/poker/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "df8ef3f14229c419a551fad7ad25ad35",
+    "url": "/poker/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "d6342af2e5aef4a2cdc5b39da012852d",
+    "url": "/poker/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "1d18de21bc62da5d9ba5b24d4523624c",
+    "url": "/poker/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "90da9b8cb201b6f333124a31dad65c39",
+    "url": "/poker/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "bed7bf5c3b14ccc0b95224ccc2673d16",
+    "url": "/poker/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "ad1a9e4a79cb794d25e05c1f6d079ba1",
+    "url": "/poker/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "8fc37cb880556e78532759cdcf7b6b9e",
+    "url": "/poker/img/icons/logo.svg"
+  },
+  {
+    "revision": "b18df4420733217183dde6732ba9771d",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "18a70fbc19db64f6c428",
+    "url": "/poker/js/app~d0ae3f07.7d74f886.js"
+  },
+  {
+    "revision": "a516fd08d1a60299aa03",
+    "url": "/poker/js/chunk-14a60960.794f214a.js"
+  },
+  {
+    "revision": "6470fa38a40379f5894a",
+    "url": "/poker/js/chunk-174cea69.cfb11a99.js"
+  },
+  {
+    "revision": "e7075da32090f159b744",
+    "url": "/poker/js/chunk-1b469fe0.f421ed23.js"
+  },
+  {
+    "revision": "154afd8a60283741d139",
+    "url": "/poker/js/chunk-2d0c7ca0.0f38f62e.js"
+  },
+  {
+    "revision": "f74d33ffbce4e7126f76",
+    "url": "/poker/js/chunk-37e1cec8.e1d35920.js"
+  },
+  {
+    "revision": "bb6a5e8d9b83fb036b23",
+    "url": "/poker/js/chunk-588296c3.c46de903.js"
+  },
+  {
+    "revision": "26a3bb7c65313534b6e4",
+    "url": "/poker/js/chunk-6f5139b6.05c873bc.js"
+  },
+  {
+    "revision": "12d0649bf2c077cd8f7f",
+    "url": "/poker/js/chunk-d3536006.57e9a945.js"
+  },
+  {
+    "revision": "b3ec4075f5d3d142cea3",
+    "url": "/poker/js/chunk-d4764c40.57a38ef1.js"
+  },
+  {
+    "revision": "dec24c0ef0a49e7528a7",
+    "url": "/poker/js/chunk-vendors~11c2601a.17a8ff58.js"
+  },
+  {
+    "revision": "b502843e5247b4441d78",
+    "url": "/poker/js/chunk-vendors~4a7e9e0b.6a8a9071.js"
+  },
+  {
+    "revision": "1669a9d1ad9e46a05259",
+    "url": "/poker/js/chunk-vendors~c6230548.b0c9be47.js"
+  },
+  {
+    "revision": "f3f17cb4dbddf5f10a42",
+    "url": "/poker/js/chunk-vendors~d2305125.556e3034.js"
+  },
+  {
+    "revision": "f89f7795619ab0ddc094",
+    "url": "/poker/js/chunk-vendors~d939e436.fe2db598.js"
+  },
+  {
+    "revision": "ca36f7daaa31078f7f6b",
+    "url": "/poker/js/chunk-vendors~e4173fa2.4117b423.js"
+  },
+  {
+    "revision": "885a5f6eb8448471b440",
+    "url": "/poker/js/chunk-vendors~f9ca8911.132ad9ec.js"
+  },
+  {
+    "revision": "7e5a635f628053bff285",
+    "url": "/poker/js/chunk-vendors~fdc6512a.0d2995df.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/poker/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3286b5ed3cea72539fc30c10855f8c76.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/poker/precache-manifest.15db1459f693624470068ac233dea6be.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: 'planning-poker'})
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
