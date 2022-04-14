2ce2872f3d40bd7e555a25496c7a064575a430dc
Luiz <lhvalentim@outlook.coom>
2022-04-14 20:44:26 -0300

    onunload test

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/poker/precache-manifest.d8e3e74824f29fc0d5a5bbd9fa5cbf3a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/poker/precache-manifest.35683f21dddab04975dedf8fbf2ac6a8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: 'planning-poker'})
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
